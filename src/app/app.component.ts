04bc7ccde42ac12e49c04c66be880cb34d783abf
LumenLumen <lucie.barbier13@gmail.com>
2025-03-23 04:38:40 +0100

    Les expressions changent git add *

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AgentDialogLineComponent } from './agent-dialog-line/agent-dialog-line.component';
 import { ZoneInputComponent } from "./zone-input/zone-input.component";
@@ -14,31 +14,44 @@ import { Reponse } from './models/reponse';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewChecked{
   title = 'Hotel';
 
-  listeMessage: Array<[string, number]> = [];
+  listeMessage: Array<[string, number, string]> = [];
 
 constructor(private agentService: AgentAPIService) {}
 
+  @ViewChild('scrollable')
+  private scrollContainer!: ElementRef;
+  
   ngOnInit(): void {
-      this.listeMessage.push(["Bonjour, je suis Epp, votre agent réceptionniste. Que puis-je faire pour vous ?", 0]);
+      this.listeMessage.push(["Bonjour, je suis Epp, votre agent réceptionniste. Que puis-je faire pour vous ?", 0, ""]);
   }
 
   myOuputData(event: any){
     console.log(event);
     let agentReponse: Reponse
-    this.listeMessage.push([event.texte,1]);
+    this.listeMessage.push([event.texte,1, ""]);
     this.agentService.postMessage({message: event.texte}).subscribe(
       response => {
         console.log('Success:', response);
         agentReponse = response as Reponse;
-        this.listeMessage.push([agentReponse.reponse,0]);
+        this.listeMessage.push([agentReponse.reponse, 0, agentReponse.emotion]);
       },
       error => {
         console.error('Error:', error);
       }
     );
+  }
+
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
+
+
+  private scrollToBottom(): void {
+    const container = this.scrollContainer.nativeElement;
+    container.scrollTop = container.scrollHeight;
   }
 
 }
